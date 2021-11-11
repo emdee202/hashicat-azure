b3970561e9f22f8885e3d6adb41e969291261a2e
Willis Hertweck <willis.hertweck@gmail.com>
2021-11-11 21:08:41 +0000

    added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/willis-company/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "Gauravv"
+  resource_group_name = "test"
   # insert required variables here
 }
